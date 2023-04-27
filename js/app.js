993fb969b645245ac836af75e2ac14a5254cd66e
Siarhei Tsihovich <siarhei.tsihovich@gmail.com>
2023-04-27 14:17:09 +0400

    add: add belarus keyboard

--- js/app.js
+++ js/app.js
@@ -31,10 +31,10 @@ const makeButton = (character, characterCapital, color = 'grey', action = undefi
   };
 } 
 
-// английская раскладка клавиатуры
+// английская и беларуская раскладка клавиатуры
 const keyboardButtonsEng = [
   [
-    makeButton('`', '~'), makeButton('1', '!'), makeButton('2', '@'), makeButton('3', '#'), makeButton('4', '$'), makeButton('5', '%'), makeButton('6', '^'), makeButton('7', '&'), makeButton('8', '('), makeButton('0', ')'), makeButton('-', '_'), makeButton('=', '+'), makeButton('Backspace', 'Backspace', 'red', backspaceAction, 2),
+    makeButton('`', '~'), makeButton('1', '!'), makeButton('2', '@'), makeButton('3', '#'), makeButton('4', '$'), makeButton('5', '%'), makeButton('6', '^'), makeButton('7', '&'), makeButton('8', '*'), makeButton('9', '('), makeButton('0', ')'), makeButton('-', '_'), makeButton('=', '+'), makeButton('Backspace', 'Backspace', 'red', backspaceAction, 2),
   ],
   
   [
@@ -42,11 +42,34 @@ const keyboardButtonsEng = [
   ],
 
   [
-    makeButton('Caps lock', 'Caps lock', 'red', capslockAction, 2), makeButton('a', 'A'), makeButton('s', 'S'), makeButton('d', 'D'), makeButton('f', 'F'), makeButton('g', 'G'), makeButton('h', 'H'), makeButton('j', 'J'), makeButton('k', 'K'), makeButton('l', 'L'), makeButton(';', ':'), makeButton("'", '"'), makeButton('Enter',  'Enter', 'red', enterAction, 2), makeButton('\/', '|'), makeButton('DEL', 'DEL', 'red', delAction, 1.5), 
+    makeButton('Caps lock', 'Caps lock', 'red', capslockAction, 2), makeButton('a', 'A'), makeButton('s', 'S'), makeButton('d', 'D'), makeButton('f', 'F'), makeButton('g', 'G'), makeButton('h', 'H'), makeButton('j', 'J'), makeButton('k', 'K'), makeButton('l', 'L'), makeButton(';', ':'), makeButton("'", '"'), makeButton('Enter',  'Enter', 'red', enterAction, 2), 
   ],
 
   [
     makeButton('Shift', 'Shift', 'red', shiftAction, 1.5), makeButton('\/', '\/'), makeButton('z', 'Z'), makeButton('x', 'X'), makeButton('c', 'C'), makeButton('v', 'V'), makeButton('b', 'B'), makeButton('n', 'N'), makeButton('m', 'M'), makeButton('n', 'N'), makeButton(',', '<'), makeButton('.', '>'), makeButton('/', '?'), makeButton('ArrowUp', 'ArrowUp', 'red', arrowUpAction), makeButton('Shift', 'Shift', 'red', shiftAction, 1.5),
+  ],
+
+  [
+    makeButton('Ctrl', 'Ctrl', 'red', ctrlAction, 1.5), makeButton('Win', 'Win'), makeButton('Alt', 'Alt', 'red', altAction), makeButton('', '', 'red', spaceAction, 5), makeButton('Alt gr', 'Alt gr'), makeButton('Ctrl', 'Ctrl', 'red', ctrlAction, 1.5), makeButton('ArrowLeft', 'ArrowLeft', 'red', arrowLeftAction), makeButton('ArrowDown', 'ArrowDown', 'red', arrowDownAction), makeButton('ArrowRight', 'ArrowRight', 'red', arrowRightAction),
+  ],
+
+];
+
+const keyboardButtonsBlr = [
+  [
+    makeButton('ё', 'Ё'), makeButton('1', '!'), makeButton('2', '"'), makeButton('3', '№'), makeButton('4', ';'), makeButton('5', '%'), makeButton('6', ':'), makeButton('7', '?'), makeButton('8', '*'), makeButton('9', '('), makeButton('0', ')'), makeButton('-', '_'), makeButton('=', '+'), makeButton('Backspace', 'Backspace', 'red', backspaceAction, 2),
+  ],
+  
+  [
+    makeButton('Tab', 'Tab'), makeButton('й', 'Й'), makeButton('ц', 'Ц'), makeButton('у', 'У'), makeButton('к', 'К'), makeButton('е', 'Е'), makeButton('н', 'Н'), makeButton('г', 'Г'), makeButton('ш', 'Ш'), makeButton('ў', 'Ў'), makeButton('з', 'З'), makeButton('х', 'Х'), makeButton("'", "'"), makeButton('\/', '/'), makeButton('DEL', 'DEL', 'red', delAction, 1.5), 
+  ],
+
+  [
+    makeButton('Caps lock', 'Caps lock', 'red', capslockAction, 2), makeButton('ф', 'Ф'), makeButton('ы', 'Ы'), makeButton('в', 'В'), makeButton('а', 'А'), makeButton('п', 'П'), makeButton('р', 'Р'), makeButton('о', 'О'), makeButton('л', 'Л'), makeButton('д', 'Д'), makeButton('ж', 'Ж'), makeButton("э", 'Э'), makeButton('Enter',  'Enter', 'red', enterAction, 2), 
+  ],
+
+  [
+    makeButton('Shift', 'Shift', 'red', shiftAction, 1.5), makeButton('\/', '\/'), makeButton('я', 'Я'), makeButton('ч', 'Ч'), makeButton('с', 'С'), makeButton('м', 'М'), makeButton('і', 'І'), makeButton('т', 'Т'), makeButton('ь', 'Ь'), makeButton('б', 'Б'), makeButton('ю', 'Ю'), makeButton('.', ','), makeButton('/', '?'), makeButton('ArrowUp', 'ArrowUp', 'red', arrowUpAction), makeButton('Shift', 'Shift', 'red', shiftAction, 1.5),
   ],
 
   [
--- js/keyboards.js
+++ js/keyboards.js
@@ -1,52 +1,6 @@
 
 
 
-// const keyboardButtonsEng = [
-//   [
-//     makeButton('`', '~'), makeButton('1', '!'), makeButton('2', '@'), makeButton('3', '#'), makeButton('4', '$'), makeButton('5', '%'), makeButton('6', '^'), makeButton('7', '&'), makeButton('8', '('), makeButton('0', ')'), makeButton('-', '_'), makeButton('=', '+'), makeButton('Backspace', 'Backspace', 'red', backspaceAction, 2),
-//   ],
-  
-//   [
-//     makeButton('Tab', 'Tab'), makeButton('q', 'Q'), makeButton('w', 'W'), makeButton('e', 'E'), makeButton('r', 'R'), makeButton('t', 'T'), makeButton('y', 'Y'), makeButton('u', 'U'), makeButton('i', 'I'), makeButton('o', 'O'), makeButton('p', 'P'), makeButton('[', '{'), makeButton(']', '}'), makeButton('\/', '|'), makeButton('DEL', 'DEL', 'red', delAction, 1.5), 
-//   ],
-
-//   [
-//     makeButton('Caps lock', 'Caps lock', 'red', capslockAction, 2), makeButton('a', 'A'), makeButton('s', 'S'), makeButton('d', 'D'), makeButton('f', 'F'), makeButton('g', 'G'), makeButton('h', 'H'), makeButton('j', 'J'), makeButton('k', 'K'), makeButton('l', 'L'), makeButton(';', ':'), makeButton("'", '"'), makeButton('Enter',  'Enter', 'red', enterAction, 2), makeButton('\/', '|'), makeButton('DEL', 'DEL', 'red', delAction, 1.5), 
-//   ],
-
-//   [
-//     makeButton('Shift', 'Shift', 'red', shiftAction, 1.5), makeButton('\/', '\/'), makeButton('z', 'Z'), makeButton('x', 'X'), makeButton('c', 'C'), makeButton('v', 'V'), makeButton('b', 'B'), makeButton('n', 'N'), makeButton('m', 'M'), makeButton('n', 'N'), makeButton(',', '<'), makeButton('.', '>'), makeButton('/', '?'), makeButton('ArrowUp', 'ArrowUp', 'red', arrowUpAction), makeButton('Shift', 'Shift', 'red', shiftAction, 1.5),
-//   ],
-
-//   [
-//     makeButton('Ctrl', 'Ctrl', 'red', ctrlAction, 1.5), makeButton('Win', 'Win'), makeButton('Alt', 'Alt', 'red', altAction), makeButton('', '', 'red', spaceAction, 5), makeButton('Alt gr', 'Alt gr'), makeButton('Ctrl', 'Ctrl', 'red', ctrlAction, 1.5), makeButton('ArrowLeft', 'ArrowLeft', 'red', arrowLeftAction), makeButton('ArrowDown', 'ArrowDown', 'red', arrowDownAction), makeButton('ArrowRight', 'ArrowRight', 'red', arrowRightAction),
-//   ],
-
-// ];
-
-// const keyboardButtonsBlr = [
-//   [
-//     makeButton('esc', 'esc'), makeButton('`', '~'), makeButton('1', '!'), makeButton('Backspace', 'Backspace', 'red', backspaceAction, 2),
-//   ],
-  
-//   [
-//     makeButton('Tab', 'Tab'), makeButton('й', 'Й'), makeButton('ц', 'Ц'), makeButton('ў', 'Ў'),
-//   ],
-
-//   [
-    
-//   ],
-
-//   [
-//     makeButton('Shift', 'Shift', 'red', shiftAction),
-//   ],
-
-//   [
-//     makeButton('Ctrl', 'Ctrl', 'red', ctrlAction,  1.5), makeButton('Alt', 'Alt', 'red', altAction, 1.5),
-//   ],
-
-// ];
-
 // export const languages = Object.freeze({
 //   'en': keyboardButtonsEng,
 //   'by': keyboardButtonsBlr,
